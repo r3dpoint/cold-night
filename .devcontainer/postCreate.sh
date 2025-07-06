9e1580029ad9a7895c61f544f1a0fef186fd509d
Naresh Sikha <3207964+r3dpoint@users.noreply.github.com>
2025-07-06 19:17:27 +0000

    Install Claude

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+go version
+npm install -g @anthropic-ai/claude-code
+
+# set a well known password for jetbrains ssh
+# new_passwd="password"
+# echo -e "${new_passwd}\n${new_passwd}" | sudo passwd "$(whoami)"
+
+# nvm alias default lts/iron
+# npm install
+# go install github.com/cosmtrek/air@latest
+
+# git config --global branch.autosetupmerge always
+# git config --global branch.autosetuprebase always
+
+# sudo apt-get update && apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
